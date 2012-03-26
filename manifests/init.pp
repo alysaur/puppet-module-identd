a09daf25a5fd882e938d5f7c1cc8f07294b3bc4f
William Van Hevelingen <blkperl@cat.pdx.edu>
2012-03-26 13:43:01 -0700

    Add freebsd support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,49 @@
 class identd {
   case $operatingsystem {
-    "ubuntu","debian": {
-      package { "identd":
-        name   => "oidentd",
-        ensure => "installed",
+    'ubuntu','debian': {
+      package { 'identd':
+        name   => 'oidentd',
+        ensure => 'installed',
       }
-      service { "identd":
-	hasstatus => false,
-	pattern   => "/usr/sbin/oidentd",
-        enable    => true,
-        ensure    => "running",
-        name      => "oidentd",
-        require   => Package["identd"];
-
+      file { 'oidentd.conf':
+        path    => '/etc/oidentd.conf',
+        source  => 'puppet:///modules/identd/oidentd.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => 0644,
+        require => Package['identd'],
+      }
+      service { 'identd':
+        name    => 'oidentd',
+        ensure  => 'running',
+        enable  => true,
+        require => File['oidentd.conf'],
+      }
+    }
+    'freebsd': {
+      package { 'identd':
+        name   => 'security/oidentd',
+        ensure => 'installed',
+      }
+      file { 'oidentd.conf':
+        path    => '/usr/local/etc/oidentd.conf',
+        source  => 'puppet:///modules/identd/oidentd.conf',
+        owner   => 'root',
+        group   => 'wheel',
+        mode    => 0644,
+        require => Package['identd'],
+      }
+      service { 'identd':
+        name    => 'oidentd',
+        ensure  => 'running',
+        enable  => true,
+        require => File['oidentd.conf'],
       }
     }
     default: {
-      notify { "ident module not configured for $operatingsystem":; }
+      notify { 'identd':
+        message => "ident module not configured for $operatingsystem",
+      }
     }
   }
 }
