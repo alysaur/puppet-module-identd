6479c50d2090f28b419804a96bc8599e89408baa
Colleen Murphy <cmurphy@cat.pdx.edu>
2012-05-10 23:38:28 -0700

    Create params file and modify init.pp to use it.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,23 @@
 class identd {
-  case $operatingsystem {
-    'ubuntu','debian': {
-      package { 'identd':
-        name   => 'oidentd',
-        ensure => 'installed',
-      }
-      file { 'oidentd.conf':
-        path    => '/etc/oidentd.conf',
-        source  => 'puppet:///modules/identd/oidentd.conf',
-        owner   => 'root',
-        group   => 'root',
-        mode    => 0644,
-        require => Package['identd'],
-      }
-      service { 'identd':
-        name      => 'oidentd',
-        ensure    => 'running',
-        enable    => true,
-        hasstatus => false,
-        require   => File['oidentd.conf'],
-      }
-    }
-    'freebsd': {
-      package { 'identd':
-        name   => 'security/oidentd',
-        ensure => 'installed',
-      }
-      file { 'oidentd.conf':
-        path    => '/usr/local/etc/oidentd.conf',
-        source  => 'puppet:///modules/identd/oidentd.conf',
-        owner   => 'root',
-        group   => 'wheel',
-        mode    => 0644,
-        require => Package['identd'],
-      }
-      service { 'identd':
-        name    => 'oidentd',
-        ensure  => 'running',
-        enable  => true,
-        require => File['oidentd.conf'],
-      }
-    }
-    default: {
-      notify { 'identd':
-        message => "ident module not configured for $operatingsystem",
-      }
-    }
+  include identd::params
+
+  package { 'identd':
+    ensure => 'installed',
+    name   => $identd::params::packagename,
+  }
+  file { 'oidentd.conf':
+    path    => $identd::params::filepath,
+    source  => $identd::params::filesource,
+    owner   => $identd::params::rootowner,
+    group   => $identd::params::rootgroup,
+    mode    => '0644',
+    require => Package['identd'],
+  }
+  service { 'identd':
+    ensure    => 'running',
+    name      => $identd::params::servicename,
+    enable    => true,
+    hasstatus => $identd::params::hasstatusval,
+    require   => File['oidentd.conf'],
   }
 }
---
+++ manifests/params.pp
@@ -0,0 +1,32 @@
+# Class: identd::params
+#
+#   The identd configuration settings.
+
+class identd::params {
+
+  case $::osfamily {
+    'Debian': {
+      $packagename = 'oidentd'
+      $filepath    = '/etc/oidentd.conf'
+      $filesource  = 'puppet:///modules/identd/oidentd.conf'
+      $rootowner   = 'root'
+      $rootgroup   = 'root'
+      $servicename = 'oidentd'
+      $hasstatusval   = false
+    }
+    'FreeBSD': {
+      $packagename = 'security/oidentd'
+      $filepath    = '/usr/local/etc/oidentd.conf'
+      $filesource  = 'puppet:///modules/identd/oidentd.conf'
+      $rootowner   = 'root'
+      $rootgroup   = 'wheel'
+      $servicename = 'oidentd'
+      $hasstatusval   = true
+    }
+    default: {
+      fail("Unsupported osfamily: ${::osfamily},\
+      module ${module_name} only supports operatingsystem \
+      Ubuntu, Debian, or FreeBSD")
+    }
+  }
+}
